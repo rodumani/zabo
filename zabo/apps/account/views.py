a9b5dd7fb992b3ede51704ce6ca604b2bc788929
yumgyun <yumgyun@sparcs.org>
2014-03-18 23:57:31 +0900

    #1 login view기능 구현

--- zabo/apps/account/views.py
+++ zabo/apps/account/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+## Create your views here.
+from django.shortcuts import redirect, render_to_response
+from django.contrib import auth
+from django.template import RequestContext
+
+def login(request):
+    next_url = request.GET.get('next', '/')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None and user.is_active:
+            auth.login(request, user)
+            return redirect(request.POST['next'])
+        else:
+            error = "Invalid login"
+        return render_to_response('login.html', {
+            'next':next_url,
+            'error':error}, context_instance=RequestContext(request))
+    return render_to_response('login.html', {'next':next_url}, context_instance=RequestContext(request))
+ Create your views here.
