08cc970553228068d59d4c6c7d9e228e9268d5a4
zoonoo <junwoopark0130@gmail.com>
2014-07-27 21:56:07 +0900

    views.py 수정

--- zabo/apps/board/views.py
+++ zabo/apps/board/views.py
@@ -18,30 +18,18 @@ def view(request, page=1):
         pass
     no_of_articles = Article.objects.count()
     articles = Article.objects.all().order_by('-written_datetime')
-        #need to make it more efficient
-    per_page = 4
-    page = int(page)
-    chosen_articles = articles[(page-1)*per_page : page*per_page]
-    chosen = []
-    for each in chosen_articles:
+    chosen = [] #article.picture.url
+    for each in articles:
         chosen.append(each.poster.all()[0].picture.url)
-    print len(chosen)
-    #other_pages = filter(lambda x: x > 0, range(int(page)-3, int(page)+4))
-    #other_pages = filter(lambda x: x < ((no_of_articles/4)+2), other_pages)
-    print no_of_articles
-    if page < 5:
-        if page+3 > (no_of_articles/4):
-            other_pages = range(1, 2+(no_of_articles-1)/4)
-        else: other_pages = range(1, page+4)
-    else:
-        if page+3 > (no_of_articles/4):
-            other_pages = range(page-3, 2+(no_of_articles-1)/4)
-        else: other_pages = range(page-3, page+4)
-    # what if the page number overflows?
-    #if page>(1+(no_of_articles-1)/4):
-    print other_pages
+
+    template = 'board/view.html'
+    page_template = 'board/view_page.html'
     ctx = {'chosen':chosen,
             'page':page,
-            'other_pages':other_pages,
-            'no_of_articles':no_of_articles}
-    return render(request, 'board/view.html', ctx)
+            'no_of_articles':no_of_articles,
+            'page_template':page_template,
+            }
+    if request.is_ajax():
+        template = page_template
+
+    return render(request, template, ctx)
