54eca832b97ac8d7893ec1d1ba3f24d33ce5e9f7
chaos <chaos@sparcs.kaist.ac.kr>
2014-08-12 06:16:40 +0900

    바뀐 context에 맞게 html 수정

--- zabo/apps/club/views.py
+++ zabo/apps/club/views.py
@@ -4,9 +4,9 @@ from zabo.apps.board.views import get_ctx
 
 def view(request, name):
     articles = Article.objects.all().order_by('id')    #TODO gather only club(name) article
-    template = 'board/view.html'
+    template = 'club_info.html'
     page_template = 'board/view_page.html'
     if request.is_ajax():
         template = page_template
 
-    return render(request, 'club_info.html', get_ctx(articles))
+    return render(request, template, get_ctx(articles))
