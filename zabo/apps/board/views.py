6def2e4d57d93db61b5351ef4949a3e04f9e0a47
Taehyun <Taehyun@Taehyun-Macui-MacBook-Air.local>
2014-08-18 01:14:51 +0900

    Change the Maximum total width of posters in one line

--- zabo/apps/board/views.py
+++ zabo/apps/board/views.py
@@ -9,7 +9,7 @@ import os
 # Create your views here.
 
 MAX_HEIGHT = 240
-PAGE_WIDTH = 1024
+PAGE_WIDTH = 1000
 MAX_WIDTH = PAGE_WIDTH * 0.9 # For good look
 def determine_space(N, length):
     remaining_width = int(MAX_WIDTH - length)
