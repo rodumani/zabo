b000350b2f06d7fa12af032603c43443c6e6a37e
chaos <chaos@sparcs.kaist.ac.kr>
2014-08-20 05:11:27 +0900

    sub picture가 없을 시 이미지 크기 수정

--- zabo/static/js/view.js
+++ zabo/static/js/view.js
@@ -7,15 +7,27 @@ function openModal(article_id){
 			article_id : article_id,
 		},
 		success: function(obj){
+			console.log(obj);
 			$("#subPictures").empty();
-			for(i=0;i<obj.sub_pictures.length;i++){
-				$("#subPictures").append('<img src="'+obj.sub_pictures[i]+'"class=subPicture>');
+			$("#mainPicture").removeAttr();
+			$("#mainPicture").attr("src", obj.main_picture.url);
+			$("#mainPicture").removeClass();
+			if(obj.sub_pictures.length == 0){
+				$("#mainPicture").addClass("largeHeightPicture");
+				if($("#mainPicture").width()>=$("#modalPictures").width()*0.9){
+					$("#mainPicture").removeClass().addClass("largeWidthPicture");
+					$("#mainPicture").css("margin-top",($("#modalPictures").height()-$("#mainPicture").height())/2);
+				}
 			}
-			$("#modalTitle").text(obj.title);
-			$("#modalWriter").text(obj.writer_name);
-			$("#mainPicture").attr("src", obj.main_picture);
+			else{
+				for(i=0;i<obj.sub_pictures.length;i++){
+					$("#subPictures").append('<img src="'+obj.sub_pictures[i].url+'"class=subPicture>');
+				}
+			}
+			$("#modalTitle").text('['+obj.category+'] '+obj.title);
+			$("#modalWriter").text(obj.writer.club_name);
 			$("#modalPeriod").text(obj.start_time + ' ~ ' + obj.end_time);
-			$("#modalContent").html(obj.content);
+			$("#modalContent").html(obj.comment);
 			window.location.href="#modalOpen";
 		}
 	});
