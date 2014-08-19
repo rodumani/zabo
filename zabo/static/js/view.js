cd5f6f5b5f3c88895a634bf9cc498c6d4b1c8c74
chaos <chaos@sparcs.kaist.ac.kr>
2014-08-19 21:45:56 +0900

    modalview main poster 실시간 반영

--- zabo/static/js/view.js
+++ zabo/static/js/view.js
@@ -2,11 +2,17 @@ function openModal(article_id){
 	$.ajax({
 		type:"GET",
 		url:"/board/get_detail/",
+		dataType:"json",
 		data:{
 			article_id : article_id,
 		},
 		success: function(obj){
-			alert(obj);
+			$("#modalTitle").text(obj.title);
+			$("#modalWriter").text(obj.writer_name);
+			$("#mainPicture").attr("src", obj.main_picture);
+			$("#modalPeriod").text(obj.start_time + ' ~ ' + obj.end_time);
+			$("#modalContent").html(obj.content);
+			window.location.href="#modalOpen";
 		}
 	});
 };
